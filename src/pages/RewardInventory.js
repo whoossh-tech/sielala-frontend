3cea5c2e58010f7f66bf971f3d18cbba347d2573
nabilaawangga <nabila.zahra11@ui.ac.id>
2024-03-04 12:24:07 +0700

    success fetch data with axios

--- src/pages/RewardInventory.js
+++ src/pages/RewardInventory.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useState, useEffect} from "react";
 // import './App.css';
 import '../static/css/RewardInventory.css';
 import '../static/css/Button.css';
+import axios from 'axios';
 
 const RewardInventory = () => {
-    const data = [
-        { name: "Anom", age: 19, gender: "Male", address: "home", email: "gmail", status: "graduated", brand: "lala"},
-        { name: "Megha", age: 19, gender: "Female", address: "home", email: "gmail", status: "graduated", brand: "lala"},
-        { name: "Subham", age: 25, gender: "Male", address: "home", email: "gmail", status: "graduated", brand: "lala"},
-    ]
+
+    const [rewardData, setRewardData] = useState([]);
+
+   useEffect(() => {
+    axios.get('http://localhost:8080/api/reward/view-all')
+    .then(res => {
+        setRewardData(res.data.data)
+    })
+   }, [])
+
+
     return (  
         <div className="RewardInventory">
             <h2>Reward Inventory</h2>
@@ -18,27 +26,17 @@ const RewardInventory = () => {
             </div>
             <table >
                 <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Address</th>
-                    <th>Email</th>
-                    <th>Status</th>
-                    <th>Brand</th>
+                    <th>Product Name</th>
+                    <th>Brand Name</th>
+                    <th>Category</th>
                 </tr>
-                {data.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td>{val.name}</td>
-                            <td>{val.age}</td>
-                            <td>{val.gender}</td>
-                            <td>{val.address}</td>
-                            <td>{val.email}</td>
-                            <td>{val.status}</td>
-                            <td>{val.brand}</td>
-                        </tr>
-                    )
-                })}
+                {rewardData.map((reward, i) => (
+                    <tr key={i}>
+                        <td>{reward.productName}</td>
+                        <td>{reward.brandName}</td>
+                        <td>{reward.category}</td>
+                    </tr>
+                ))}
             </table>
         </div>
     );
