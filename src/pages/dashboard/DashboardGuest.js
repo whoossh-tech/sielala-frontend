d1f95bd28c2599109e3e1fabec5a0376cb399c63
nabilaawangga <nabila.zahra11@ui.ac.id>
2024-04-27 17:19:07 +0700

    modify dashboard guest title text

--- src/pages/dashboard/DashboardGuest.js
+++ src/pages/dashboard/DashboardGuest.js
@@ -46,11 +46,37 @@ const DashboardGuest = () => {
             <NavbarGuest />
             <style>{reynaldoStyles}</style>
             <br></br>
-            <h1>Hello, this is guest's dashboard</h1>
-
             <br></br>
+            <h1 className="text-7xl">Lala Market</h1>
+            <h1 className="text-2xl">Sexiest, Hottest, Endless Sale</h1>
+            <h2>A Fashion Curated Bazaar Featuring 50+ Brands with Discounts Up to 90%</h2>
+            <br></br>
+            <div className="event-card bg-primary-10 shadow-md rounded-lg py-3 px-8 my-5">
+                    <div className="flex justify-between">
+                        <div className='text-left'>
+                            <h1 className='text-2xl my-2'>tes q</h1>
+                            <p className="text-md mb-2 text-center">
+                                📆 tes 2 | 📍 tes 3
+                            </p>
+                        </div>
+
+                        <div className="5 flex items-center gap-0 mx-3">
+                            <div className="flex space-x-4">
+                                <button className="button-pink text-sm" >
+                                    Register as Visitor
+                                </button>
+
+                                    <button className="button-green text-sm" >
+                                        Register as Tenant
+                                    </button>
+                            </div>
+                        </div>
+                    </div>
+            </div>
             <h1 className="text-lg upcoming-title font-bold text-left">UPCOMING EVENT</h1>
 
+            <br></br>
+            
             {events && events.length > 0 ? (
               events.map((event, i) => (
                 <div className="event-card bg-primary-10 shadow-md rounded-lg py-3 px-8 my-5">
