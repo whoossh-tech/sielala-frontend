34350807b2a15136a6d2b03cc162ff7cd207dd3b
Shayna Putri Fitria <shaynaaapp@gmail.com>
2024-05-03 01:22:43 +0700

    sidebar per role & fix styling

--- src/pages/dashboard/Sidebar.js
+++ src/pages/dashboard/Sidebar.js
@@ -5,14 +5,20 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../../static/css/Style.css';
 import logo from "../../assets/logo-sielala.png";
 import element from "../../assets/element.png";
-
+import { useAuth } from "../../pages/auth/AuthContext";
 
 const Sidebar = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
   const role = localStorage.getItem('role');
+  const { logout } = useAuth(); 
 
   const toggleSidebar = () => {
     setSidebarVisible(!sidebarVisible);
+  };
+
+  const handleLogout = () => {
+    logout(); 
+    window.location.href = '/';
   };
 
   return (
@@ -37,14 +43,12 @@ const Sidebar = () => {
         </div>
       ) : (
         <div className="flex items-center gap-x-1 my-8 mx-3">
-          {/* <i className='bx bxs-user-circle'></i> */}
           <img
-              src={element} 
-              alt="SieLALA Element"
-              className="object-cover"
-              style={{ height: "42px", width: "40px" }}
-            />
-
+            src={element} 
+            alt="SieLALA Element"
+            className="object-cover"
+            style={{ height: "42px", width: "40px" }}
+          />
         </div>
       )}
 
@@ -60,81 +64,197 @@ const Sidebar = () => {
           </li>
 
           <li>
-            <a href="user-list">
-              <i className='bx bx-user'></i> {/* Corrected icon class name */}
+            <a href="/user-list">
+              <i className='bx bx-user'></i>
               <span className="text">User Management</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-calendar'></i> {/* Corrected icon class name */}
+            <a href="/event">
+              <i className='bx bx-calendar'></i> 
               <span className="text">Event Management</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-group'></i> {/* Corrected icon class name */}
-              <span className="text">Visitor Management</span>
+            <a href="/visitor">
+              <i className='bx bx-group'></i>
+              <span className="text">Visitor Data Report</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-envelope'></i> {/* Corrected icon class name */}
-              <span className="text">Bulk Email</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              <i className='bx bx-archive'></i> {/* Corrected icon class name */}
+            <a href="/reward-inventory">
+              <i className='bx bx-archive'></i>  
               <span className="text">Reward Inventory</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-gift'></i> {/* Corrected icon class name */}
+            <a href="/reward-redemption-history">
+              <i className='bx bx-gift'></i>  
               <span className="text">Reward Redemption</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-printer'></i> {/* Corrected icon class name */}
+            <a href="/invoice">
+              <i className='bx bx-printer'></i>  
               <span className="text">Invoice Management</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-phone'></i> {/* Corrected icon class name */}
+            <a href="/contact">
+              <i className='bx bx-phone'></i>  
               <span className="text">Partnership</span>
             </a>
           </li>
 
           <li>
-            <a href="#">
-              <i className='bx bx-store'></i> {/* Corrected icon class name */}
+            <a href="/tenant-applicant">
+              <i className='bx bx-store'></i>  
               <span className="text">Tenant Applicant</span>
             </a>
           </li>
 
+          <li>
+            <a href="/bulk-email">
+              <i className='bx bx-envelope'></i>  
+              <span className="text">Bulk Email</span>
+            </a>
+          </li>
+        </ul>
+      )}
 
+      {(role === "BISDEV") && (
+        <ul className="side-menu top">
+          <li className="active">
+            <a href="/dashboard">
+              <i className='bx bxs-dashboard'></i>
+              <span className="text">Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/event">
+              <i className='bx bx-calendar'></i> 
+              <span className="text">Event Management</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/visitor">
+              <i className='bx bx-group'></i>
+              <span className="text">Visitor Data Report</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/bulk-email">
+              <i className='bx bx-envelope'></i>  
+              <span className="text">Bulk Email</span>
+            </a>
+          </li>
+        </ul>
+      )}
+
+      {(role === "FINANCE") && (
+        <ul className="side-menu top">
+          <li className="active">
+            <a href="/dashboard">
+              <i className='bx bxs-dashboard'></i>
+              <span className="text">Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/invoice">
+              <i className='bx bx-printer'></i>  
+              <span className="text">Invoice Management</span>
+            </a>
+          </li>
+        </ul>
+      )}
+
+      {(role === "OPERATION") && (
+        <ul className="side-menu top">
+          <li className="active">
+            <a href="/dashboard">
+              <i className='bx bxs-dashboard'></i>
+              <span className="text">Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/reward-inventory">
+              <i className='bx bx-archive'></i>  
+              <span className="text">Reward Inventory</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/reward-redemption-history">
+              <i className='bx bx-gift'></i>  
+              <span className="text">Reward Redemption</span>
+            </a>
+          </li>
+        </ul>
+      )}
+
+      {(role === "PARTNERSHIP") && (
+        <ul className="side-menu top">
+          <li className="active">
+            <a href="/dashboard">
+              <i className='bx bxs-dashboard'></i>
+              <span className="text">Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/invoice">
+              <i className='bx bx-printer'></i>  
+              <span className="text">Invoice Management</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/contact">
+              <i className='bx bx-phone'></i>  
+              <span className="text">Partnership</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/tenant-applicant">
+              <i className='bx bx-store'></i>  
+              <span className="text">Tenant Applicant</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="/bulk-email">
+              <i className='bx bx-envelope'></i>  
+              <span className="text">Bulk Email</span>
+            </a>
+          </li>
         </ul>
       )}
 
       {/* Log Out Content */}
       <ul class="side-menu">
-        <li>
-          <a href="#" class="logout">
+        <li style={{ alignItems: "left" }}>
+          <a class="logout" onClick={handleLogout} 
+            style={{ cursor: 'pointer' }}
+          >
             <i class='bx bxs-log-out-circle' ></i>
             <span class="text">Logout</span>
           </a>
         </li>
       </ul>
+
+      <br></br>
 
     <script src="script.js"></script>
 
